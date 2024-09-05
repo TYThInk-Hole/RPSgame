e74c729ac74c2f21e4ea702ae207b369f869d833
TYThInk-Hole <kty6689@naver.com>
2024-09-05 20:51:46 +0900

    ~ing

--- RPS_HSI.jl
+++ RPS_HSI.jl
@@ -1,6 +1,6 @@
 using Random, HDF5, Printf
 
-function RPS_inter_individualtt(Lsize, reproduction_rate, selection_rate, mobility, para, rn)
+function RPS_HSI(Lsize, reproduction_rate, selection_rate, mobility, para, rn)
     start_time = time()
     Random.seed!(rn)
 
@@ -14,19 +14,13 @@ function RPS_inter_individualtt(Lsize, reproduction_rate, selection_rate, mobili
     r2 = selection_rate / (reproduction_rate + eps)
     r3 = eps / (reproduction_rate + selection_rate + eps)
 
-    # Define neighbor offset
-    A = [1 0; -1 0; 0 1; 0 -1]
+    # Define neighbor offsets
+    neighbor_shifts = [1 0; -1 0; 0 1; 0 -1]
 
     # Setup HDF5 file
-    file_dir = "/Volumes/yoonOWC/RPS_inter_test.h5"
-    dataset1 = "/$(rn)/Lattice"
-    dataset2 = "/$(rn)/NumS"
-
-    # Create HDF5 datasets
-    h5open(file_dir, "cw") do f
-        create_dataset(f, dataset1, datatype(Float64), dataspace(Lsize, Lsize, 1000000); chunk=(Lsize, Lsize, 1))
-        create_dataset(f, dataset2, datatype(Float64), dataspace(1000000, 3); chunk=(1, 3))
-    end
+    file_dir = "/Volumes/yoonJL/RPS_inter_test.h5"
+    dataset1 = "/HSI/$(rn)"
+    dataset2 = "/NumS/$(rn)"
 
     generation = 0
     Flag = true
@@ -42,7 +36,7 @@ function RPS_inter_individualtt(Lsize, reproduction_rate, selection_rate, mobili
         rr = rand(1:4, Lsize^2)
 
         # Compute the neighbor indices with boundary condition handling
-        Cpre = mod1.(R .+ A[rr, :] .- 1, Lsize)
+        Cpre = mod1.(R .+ neighbor_shifts[rr, :] .- 1, Lsize)
         C = sub2ind(Lsize, Cpre[:, 1], Cpre[:, 2])
         R_idx = sub2ind(Lsize, R[:, 1], R[:, 2])
 
@@ -94,11 +88,20 @@ function RPS_inter_individualtt(Lsize, reproduction_rate, selection_rate, mobili
         nC = count(==(3), Lattice)
         nExt = sum([nA, nB, nC] .== 0)
 
+        # Compute HSI
+        A_rows, A_cols = findall(x -> x == 1, Lattice) |> collect
+        B_rows, B_cols = findall(x -> x == 2, Lattice) |> collect
+        C_rows, C_cols = findall(x -> x == 3, Lattice) |> collect
+
+        HSI_A = compute_HSI(Lattice, A_rows, A_cols, neighbor_shifts, Lsize, [2, 3])
+        HSI_B = compute_HSI(Lattice, B_rows, B_cols, neighbor_shifts, Lsize, [1, 3])
+        HSI_C = compute_HSI(Lattice, C_rows, C_cols, neighbor_shifts, Lsize, [1, 2])
+
         # Write data to HDF5 file
         h5open(file_dir, "r+") do f
             dset1 = f[dataset1]
             dset2 = f[dataset2]
-            dset1[:, :, generation] = Lattice
+            dset1[:, :, generation] = [HSI_A, HSI_B, HSI_C]
             dset2[generation, :] = [nA, nB, nC]
         end
 
@@ -117,12 +120,34 @@ function sub2ind(Lsize, row, col)
     return (col .- 1) .* Lsize .+ row
 end
 
+# Helper function to compute HSI
+function compute_HSI(Lattice, rows, cols, neighbor_shifts, Lsize, prey_species)
+    num_cells = length(rows)
+
+    if num_cells == 0
+        return 0.0
+    end
+
+    neighbors = zeros(Int, num_cells, size(neighbor_shifts, 1))
+
+    for k in 1:size(neighbor_shifts, 1)
+        neighbor_rows = mod1.(rows .+ neighbor_shifts[k, 1] .- 1, Lsize)
+        neighbor_cols = mod1.(cols .+ neighbor_shifts[k, 2] .- 1, Lsize)
+        neighbors[:, k] = Lattice[neighbor_rows .+ (neighbor_cols .- 1) .* Lsize]
+    end
+
+    prey_count = sum(ismember.(neighbors, prey_species), dims=2)
+    empty_count = sum(neighbors .== 0, dims=2)
+
+    return mean((prey_count .+ empty_count) / 4)
+end
+
 # Example usage:
 Lsize = 200
 reproduction_rate = 1.0
 selection_rate = 1.0
 mobility = 30
 para = 3.0
-for rn in 1:100
-    RPS_inter_individualtt(Lsize, reproduction_rate, selection_rate, mobility, para, rn)
+for rn in 1:1
+    RPS_HSI(Lsize, reproduction_rate, selection_rate, mobility, para, rn)
 end
