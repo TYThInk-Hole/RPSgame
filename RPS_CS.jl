1d2e6c710fe1b9956bd1ca13404cf680306f3f60
TYThInk-Hole <kty6689@naver.com>
2024-09-12 02:46:38 +0900

    Final -v of multi threading

--- RPS_CS.jl
+++ RPS_CS.jl
@@ -1,4 +1,4 @@
-using Random, HDF5, Printf, Statistics, SparseArrays, StatsBase, Base.Threads, Distributed
+using Random, HDF5, Printf, Statistics, SparseArrays, StatsBase, Base.Threads
 
 function RPS_intra(Lsize, reproduction_rate, selection_rate, mobility, intra1, intra2, intra3, ext, para, rn)
     start_time = time()
@@ -31,6 +31,7 @@ function RPS_intra(Lsize, reproduction_rate, selection_rate, mobility, intra1, i
     file_dir = "/home/ty/Desktop/yoonD/RPS/intra/RPS_intra_$rn.h5"
     dataset1 = "$intra1/Histogram/$rn"
     dataset2 = "$intra1/NumS/$rn"
+    # dataset3 = "$intra1/Trace/$rn"
 
     # HDF5 파일 열기 및 데이터셋 확인/생성
     h5open(file_dir, "cw") do f
@@ -45,6 +46,12 @@ function RPS_intra(Lsize, reproduction_rate, selection_rate, mobility, intra1, i
             delete_object(f, dataset2)
         end
         create_dataset(f, dataset2, Float64, ((1, 3), (-1, 3)); chunk=(1, 3))
+
+        # dataset3 처리
+        # if haskey(f, dataset3)
+        #     delete_object(f, dataset3)
+        # end
+        # create_dataset(f, dataset3, Float64, ((1, Lsize, Lsize), (-1, Lsize, Lsize)); chunk=(1, Lsize, Lsize))
     end
 
     generation = 0
@@ -184,6 +191,14 @@ function RPS_intra(Lsize, reproduction_rate, selection_rate, mobility, intra1, i
             dset2[new_size, :] = [nA, nB, nC]
         end
 
+        # Save Trace data
+        # h5open(file_dir, "r+") do f
+        #     dset3 = f[dataset3]
+        #     curr_size = size(dset3, 1)
+            # new_size = curr_size + 1
+            # HDF5.set_extent_dims(dset3, (new_size, Lsize, Lsize))
+            # dset3[new_size, :, :] = Trace
+        # end
         @printf("rn=%d, species=%d, %d, %d, nExt=%d, generation=%d\n", rn, nA, nB, nC, nExt, generation)
 
         # Stop if extinction or generation limit reached
@@ -247,23 +262,11 @@ end
 # Get parameters
 Lsize, reproduction_rate, selection_rate, mobility, intra1, intra2, intra3, ext, para, rn_start, rn_end = get_parameters()
 
-# 병렬 처리를 위한 워커 추가
-addprocs(Sys.CPU_THREADS - 1)
-
-# 필요한 모듈을 모든 워커에 로드
-@everywhere using Random, HDF5, Printf, Statistics, SparseArrays, StatsBase
-
-# RPS_intra 함수를 모든 워커에 정의
-@everywhere function RPS_intra(Lsize, reproduction_rate, selection_rate, mobility, intra1, intra2, intra3, ext, para, rn)
-    # 여기에 RPS_intra 함수의 전체 내용을 복사-붙여넣기 하세요
-end
-
-# 병렬 실행
-@sync @distributed for rn in rn_start:rn_end
+# Main loop for simulations
+# for rn in rn_start:rn_end
+#     RPS_intra(Lsize, reproduction_rate, selection_rate, mobility, intra1, intra2, intra3, ext, para, rn)
+# end
+# Main loop parallelized using Threads.@threads
+Threads.@threads for rn in rn_start:rn_end
     RPS_intra(Lsize, reproduction_rate, selection_rate, mobility, intra1, intra2, intra3, ext, para, rn)
-end
-
-# 추가된 워커 제거
-for i in workers()
-    rmprocs(i)
 end
